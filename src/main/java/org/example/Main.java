82164628e31300b71af9d94e7e71a73580de17aa
sedlami2 <sedlami2@uhk.cz>
2026-02-09 12:13:55 +0100

    Druhá úprava

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             i =+ 2;
             System.out.println("i = " + i);
+            i =- 3;
+            System.out.println("i = " + i);
         }
     }
 }
